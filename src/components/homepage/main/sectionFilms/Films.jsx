738130063e9201146f5dd3395c36f221f507f95e
Ilya Lukyanov <lukyanov776@gmail.com>
2025-12-26 22:13:24 +0300

    skeleton + refactoring

--- src/components/homepage/main/sectionFilms/Films.jsx
+++ src/components/homepage/main/sectionFilms/Films.jsx
@@ -1,24 +1,22 @@
 import Card from '../../../../ui/Card/Card'
-import Pagination from '../../../../ui/Pagination/Pagination'
 import styles from './style.module.css'
 
 const Films = (Props) => {
   const{
     films,
-    totalPages,
-    handlePageClick,
-    currentPage
   } = Props
 
   return (
-  <section className={styles.section}>
-    <h1>Top Films</h1>
+  <section >
     <div className={styles.list}>
       {films.map(film => <Card key={film.imdbID} film={film}/>)}
     </div>
-    <Pagination totalPages={totalPages} handlePageClick={handlePageClick} currentPage={currentPage}/>
   </section>
   )
 }
 
 export default Films
+
+
+
+
